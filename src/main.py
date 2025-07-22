2333ae523a012fff805ff0859cfed1635e70006b
Soufiane Ghenimi <Soufiane@SGH-MAC-2025.local>
2025-07-22 06:23:54 +0100

    documentation des methodes

--- src/main.py
+++ src/main.py
@@ -1,23 +1,32 @@
-from typing import List
+from typing import List, Any, Coroutine
 
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI(title="RPN Calculator")
 
-@app.get("/health")
-async def health():
-    return "OK"
+# @app.get("/health")
+# async def health():
+#     return "OK"
 
 # pile de la calculatrice
 rpn_stack: List[int] = []
 
 @app.get("/current_stack")
-async def current_stack():
+async def current_stack()-> dict[str, list[int]]:
+    """
+    Returns the current stack
+    :return: list of stack numbers
+    """
     return {"stack": rpn_stack}
 
 @app.post("/add_item")
-async def add_item(item: int):
+async def add_item(item: int)-> dict[str, list[int]]:
+    """
+    Adds an item to the stack
+    :param item:
+    :return: list of stack numbers
+    """
     rpn_stack.append(item)
     return {"stack": rpn_stack}
 
